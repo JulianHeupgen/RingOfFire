a2c625c986a683ba9c4b1277982f7c4c18e81189
JulianHeupgen <julianheupgen@web.de>
2024-02-09 17:34:34 +0100

    added card stack

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -9,9 +10,13 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+  constructor(private router: Router) {}
+  
+
   newGame() {
     // Start game
+    this.router.navigateByUrl('/game');
   }
 
-
 }
+
