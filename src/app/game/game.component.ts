f3ee7d9b75de850df88462f23890db25d29a42b8
JulianHeupgen <julianheupgen@web.de>
2024-02-11 15:09:41 +0100

    finished card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -13,7 +13,7 @@ import { Game } from '../../models/game';
 export class GameComponent {
 
   pickCardAnimation = false;
-  currentCard: string = '';
+  currentCard: string | undefined = '';
   game: Game = new Game();
 
   ngOnInit(): void {
@@ -29,10 +29,12 @@ export class GameComponent {
       this.currentCard = this.game.stack.pop();
       console.log(this.currentCard);
       this.pickCardAnimation = true;
-
+    
       setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        console.log(this.game);
         this.pickCardAnimation = false;
-      }, 1500);
+      }, 1000);
     }
   }
 
