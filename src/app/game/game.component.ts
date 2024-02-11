1700cd0f8b7cb3706d85bdfb7cfe66b06774c10e
JulianHeupgen <julianheupgen@web.de>
2024-02-11 11:46:33 +0100

    added class Game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,19 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Game],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
 
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+
+  }
+
+  ngOnIinit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
   }
 
 }
+
