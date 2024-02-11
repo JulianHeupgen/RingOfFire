60fa4f969350d36b726e23fe3e8b30bb43c76cf6
JulianHeupgen <julianheupgen@web.de>
2024-02-11 13:55:25 +0100

    show next card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,32 +2,38 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
 
+
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, Game],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
 
   pickCardAnimation = false;
-  game: Game;
+  currentCard: string = '';
+  game: Game = new Game();
 
-  constructor() {
-
-  }
-
-  ngOnIinit(): void {
+  ngOnInit(): void {
     this.newGame();
   }
 
   newGame() {
-    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 
 }
