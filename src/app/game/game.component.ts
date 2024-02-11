c6c63068e539f028fec3f315848558b45d69df97
JulianHeupgen <julianheupgen@web.de>
2024-02-11 16:09:23 +0100

    added first player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -16,23 +17,14 @@ export class GameComponent {
   currentCard: string | undefined = '';
   game: Game = new Game();
 
-  ngOnInit(): void {
-    this.newGame();
-  }
-
-  newGame() {
-    console.log(this.game);
-  }
 
   takeCard() {
     if(!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop();
-      console.log(this.currentCard);
       this.pickCardAnimation = true;
     
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
-        console.log(this.game);
         this.pickCardAnimation = false;
       }, 1000);
     }
